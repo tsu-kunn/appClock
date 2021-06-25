c6afb98bd777acfc7a367c14ec191ecc113e9e8a
Tsuyoshi.A <perilla.k@gmail.com>
2021-06-25 18:32:16 +0900

    Preparation to Redux

---
+++ src/store/Actions.js
@@ -0,0 +1,11 @@
+export const Actions = {
+    increase() {
+        return { type: "INCREASE_COUNT" };
+    },
+    decrease() {
+        return { type: "DECREASE_COUNT" };
+    },
+    changeImage() {
+        return { type: "CHANGE_IMAGE" };
+    }
+}
